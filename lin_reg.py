79d76fd2aa82545a7e90d9c1f21b6ed191e4cae9
sabernn <60275211+sabernn@users.noreply.github.com>
2020-04-19 02:20:31 -0500

    implementing and testing the loss function
    Item 2 is checked!

--- lin_reg.py
+++ lin_reg.py
@@ -13,6 +13,20 @@ import csv
 import matplotlib.pyplot as plt
 
 
+# Declaring the loss function
+def loss_func(inp,out,theta):
+    m=len(inp)
+    J=0
+    for i in range(m):
+        J+=(theta[0]+theta[1]*inp[i]-out[i])**2
+
+    return J/2/m
+
+
+
+
+
+
 # Initializing input and output vectors
 x=[]
 y=[]
@@ -27,6 +41,12 @@ with open('data//KCSmall2.csv', newline='') as f:
 # print(x)
 
 # print(dir(plt))
+theta1=[0,0]
+theta2=[-1,20]
+
+print(loss_func(x,y,theta1))
+print(loss_func(x,y,theta2))
+
 
 
 ######## PLOTTING ########
