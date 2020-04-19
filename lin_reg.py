06ba75239c613fb197337237604c0cc42b600b2c
sabernn <60275211+sabernn@users.noreply.github.com>
2020-04-19 01:54:40 -0500

    importing a csv file

--- lin_reg.py
+++ lin_reg.py
@@ -7,9 +7,21 @@
 # Importing packages
 import numpy as np
 import pandas as pd
+import csv
+import matplotlib.pyplot as plt
 
 
-
+# Initializing input and output vectors
+x=[]
+y=[]
 # Importing data
+with open('data//KCSmall2.csv', newline='') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        x.append(float(row[0]))
+        y.append(float(row[1]))
 
+# print(type(x[0]))
+# print(x)
 
+dir(plt)
